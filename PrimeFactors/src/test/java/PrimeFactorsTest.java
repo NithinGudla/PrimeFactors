328d47b631dede50e6a41728795405a1bb28cd0b
NithinGudla <47893203+NithinGudla@users.noreply.github.com>
2021-01-04 16:23:17 +0530

    Add files via upload

---
+++ PrimeFactors/src/test/java/PrimeFactorsTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static java.util.Collections.emptyList;
+
+public class PrimeFactorsTest {
+
+    @Test
+    void thereAreNoPrimeFactorsFor1() {
+        Assertions.assertEquals(emptyList(), new PrimeFactors().of(1));
+    }
+}
